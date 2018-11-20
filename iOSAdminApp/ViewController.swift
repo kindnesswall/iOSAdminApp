87b09f5dfae11b30cac430fdd3082b5c23e7ca9c
Hamed.Gh <h.ghadirian@outlook.com>
2018-11-20 13:06:08 +0330

    Hello World :)

---
+++ iOSAdminApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSAdminApp
+//
+//  Created by Hamed.Gh on 11/20/18.
+//  Copyright © 2018 MacBook Pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
